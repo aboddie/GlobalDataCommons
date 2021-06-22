f10aa6039be88e685a61cc740c6f07e3e888a661
Allen <64019758+aboddie@users.noreply.github.com>
2021-06-22 13:38:48 -0400

    Add files via upload

--- globaldatacommons/__init__.py
+++ globaldatacommons/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/database/nsdp.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
