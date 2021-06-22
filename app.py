b042c43a271fdc3c5a4ed333e6f906bc7e433409
Allen <64019758+aboddie@users.noreply.github.com>
2021-06-22 13:45:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-#from globaldatacommons import app
-#
-#if __name__ == '__main__':
-#    app.run(debug=True)
-from flask import Flask
-app = Flask(__name__)
+from globaldatacommons import app
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+if __name__ == '__main__':
+    app.run(debug=True)
