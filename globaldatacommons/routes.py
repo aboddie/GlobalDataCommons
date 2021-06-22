fe70c7c4f003bb79d82d1a7782a12c30aed4701a
Allen <64019758+aboddie@users.noreply.github.com>
2021-06-22 15:31:56 -0400

    Update routes.py

--- globaldatacommons/routes.py
+++ globaldatacommons/routes.py
@@ -5,9 +5,5 @@ from globaldatacommons import app
 
 @app.route("/")
 @app.route("/home")
-def home():
-    return render_template('home.html')
-
-@app.route("/about")
 def about():
     return "<h1>Allen made this!<h1>"
