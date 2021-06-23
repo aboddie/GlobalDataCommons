18e89b9ebc43d31a967fe86681294a14f21af49a
Allen <64019758+aboddie@users.noreply.github.com>
2021-06-23 09:30:45 -0400

    Update __init__.py

--- globaldatacommons/__init__.py
+++ globaldatacommons/__init__.py
@@ -1,8 +1,11 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLAZURECONNSTR_GDC_database']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
