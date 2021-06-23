9fa091652db08bd09ad882fd7adb8e8d2531c0ea
Allen <64019758+aboddie@users.noreply.github.com>
2021-06-23 10:19:02 -0400

    Update routes.py

--- globaldatacommons/routes.py
+++ globaldatacommons/routes.py
@@ -5,6 +5,21 @@ from collections import Counter
 from globaldatacommons.models import Country, Categories, Series
 from globaldatacommons import app
 
+
+def pullindicators(countryname):
+    indicatorlist = []
+    inputfilelocation = 'globaldatacommons/static/data.csv' 
+    with open(inputfilelocation, 'r', encoding='utf-8-sig') as csvfileread:
+        filereader = csv.DictReader(csvfileread, delimiter=',') 
+        for row in filereader:
+            if countryname == row['Country']:
+                indicatorlist.append({'description': row['Description'], 'inSDMX': row['inSDMX'], 'indent': row['Level'], 'data': row['Data'], 'sdmx_data': row['SDMX_Data'], 'dates': row['Dates'], 'source': row['Source']})           
+    return indicatorlist
+
+def pullcountryname(countrycode):
+    name = Country.query.filter(Country.code==countrycode).first().name 
+    return name
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -16,6 +31,21 @@ def about():
 
 @app.route('/countryview/<country>')
 def country_pages(country):
+    if country == 'AL1':
+        countryindicators = pullindicators('Albania')
+        mydomain = "Consumer Price Index"
+        return render_template('ALB_JAM.html', chart='TRUE', title=country, domain=mydomain, country=countryindicators)
+    elif country == 'JA1':
+        countryindicators = pullindicators('Jamaica')
+        mydomain = "Consumer Price Index"
+        return render_template('ALB_JAM.html', chart='TRUE', title=country, domain=mydomain, country=countryindicators)
+    else:
+        datadomains = Categories.query.filter(Categories.countrycode==country).all()
+        country_name = pullcountryname(country)
+        return render_template('CountryPages.html', title=country_name, listitems=datadomains)
+
+@app.route('/dataview/<country>/<domain>')
+def country_domain(country,domain):
     return "<h1>Allen made this!<h1>"
 
 @app.route("/report")
