f921161bf70c146e856455eb862f61439bf730f0
Allen Boddie <allen.boddie1@gmail.com>
2021-09-23 14:44:18 -0400

    Clean up html

--- globaldatacommons/__init__.py
+++ globaldatacommons/__init__.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLAZURECONNSTR_GDC_database']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
