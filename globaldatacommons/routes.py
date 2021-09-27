4f1800c83e4201155eb91582304b01a2a3442b75
Allen Boddie <allen.boddie1@gmail.com>
2021-09-27 10:13:46 -0400

    remove title and clean up so CSS

--- globaldatacommons/routes.py
+++ globaldatacommons/routes.py
@@ -5,50 +5,22 @@ from .models import Country, Categories, Series
 from . import app
 
 
-def pullindicators(countryname):
-    indicatorlist = []
-    inputfilelocation = 'globaldatacommons/static/data.csv' 
-    with open(inputfilelocation, 'r', encoding='utf-8-sig') as csvfileread:
-        filereader = csv.DictReader(csvfileread, delimiter=',') 
-        for row in filereader:
-            if countryname == row['Country']:
-                indicatorlist.append({'description': row['Description'], 'inSDMX': row['inSDMX'], 'indent': row['Level'], 'data': row['Data'], 'sdmx_data': row['SDMX_Data'], 'dates': row['Dates'], 'source': row['Source']})           
-    return indicatorlist
-
-def pullcountryname(countrycode):
-    name = Country.query.filter(Country.code==countrycode).first().name 
-    return name
-
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', countries=Country.query.order_by(Country.name).all())
 
-@app.route("/about")
-def about():
-    return "<h1>Allen made this!<h1>"
-
 @app.route('/countryview/<countrycode>')
 def country_pages(countrycode):
-    if countrycode == 'AL1':
-        countryindicators = pullindicators('Albania')
-        mydomain = "Consumer Price Index"
-        return render_template('ALB_JAM.html', chart='TRUE', title=countrycode, domain=mydomain, country=countryindicators)
-    elif countrycode == 'JA1':
-        countryindicators = pullindicators('Jamaica')
-        mydomain = "Consumer Price Index"
-        return render_template('ALB_JAM.html', chart='TRUE', title=countrycode, domain=mydomain, country=countryindicators)
-    else:
-        datadomains = Categories.query.filter(Categories.countrycode==countrycode).all()
-        country_info = Country.query.filter(Country.code==countrycode).first()
-        country_name = country_info.name
-        return render_template('CountryPages.html', title=country_name, listitems=datadomains, country_info=country_info)
+    return render_template('CountryPages.html', 
+                            listitems=Categories.query.filter(Categories.countrycode==countrycode).all(), 
+                            country_info=Country.query.filter(Country.code==countrycode).first())
 
-@app.route('/dataview/<country>/<domain>')
-def country_domain(country,domain):
-    countrydata = Series.query.filter(Series.countrycode==country,Series.categorycode==domain)
-    country_name = pullcountryname(country)
-    return render_template('CountryDomains.html', title=country_name, country=countrydata)
+@app.route('/dataview/<countrycode>/<domain>')
+def country_domain(countrycode,domain):
+    countrydata = Series.query.filter(Series.countrycode==countrycode,Series.categorycode==domain)
+    country_info = Country.query.filter(Country.code==countrycode).first()
+    return render_template('CountryDomains.html', country_info=country_info, country=countrydata)
 
 @app.route("/report")
 def report():
@@ -76,6 +48,8 @@ def country_report(country):
         total[3] = f'{total[0]/total[2]:.1%}'
     except ZeroDivisionError:
         total[3] = "N.A."
-    country_name = pullcountryname(country)
-    return render_template('CountryReport.html', title=country_name, country=countrydata, total=total)
+    return render_template('CountryReport.html', country=countrydata, total=total)
 
+@app.route("/about")
+def about():
+    return "<h1>Allen made this!<h1>"
