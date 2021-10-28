2beefbefa8035c2c7d756e0145e4ca84a71faaf2
Allen Boddie <allen.boddie1@gmail.com>
2021-10-28 14:31:38 -0400

    Add copy codes button

--- globaldatacommons/__init__.py
+++ globaldatacommons/__init__.py
@@ -4,14 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask import Flask
 
-from .models import metadata_obj
-#from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
-
 
 engine = create_engine(os.environ['SQLAZURECONNSTR_GDC_database'])
 Session = sessionmaker(engine)
@@ -19,8 +15,4 @@ Session = sessionmaker(engine)
 review_engine = create_engine(os.environ['SQLAZURECONNSTR_GDC_database_STG'])
 ReviewSession = sessionmaker(review_engine)
 
-
-
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#db = SQLAlchemy(app)
 from . import routes
